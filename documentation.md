08ce3e952289450d38f59120a90e50f6cb9974fe
Rebecca Medina (RIT Student) <rcm3517@ad.rit.edu>
2019-04-03 10:41:49 -0400

    Sass, gulp, and change some images

---
+++ documentation.md
@@ -0,0 +1,4 @@
+https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiXs56ejLThAhXETd8KHYbiBmsQjRx6BAgBEAU&url=https%3A%2F%2Fwww.teepublic.com%2Fkids-long-sleeve-t-shirt%2F3821775-a-chibi-cat-cute&psig=AOvVaw1AZnOIaC78TtepbnEEuthw&ust=1554386796845761 
+cat img 
+
+https://cdn130.picsart.com/240333569031212.png?c256x256
---
+++ gulpFile.js
@@ -0,0 +1,43 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const babel = require('gulp-babel');
+const nodemon = require('gulp-nodemon');
+const eslint = require('gulp-eslint');
+
+gulp.task('sass', () => {
+    gulp.src('./scss./style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./hosted'));
+});
+
+gulp.task('js', () => {
+    gulp.src('./client/*.js')
+        .pipe(babel({
+            presets: ['env', 'react']
+        }))
+        .pipe(gulp.dest('./hosted/'));
+});
+
+gulp.task('lint', () => {
+    gulp.src('./server/*.js')
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('build', () => {
+    gulp.start('sass');
+    gulp.start('js');
+    gulp.start('lint');
+});
+
+gulp.task('watch', () => {
+    gulp.watch('./scss/style.scss', ['sass']);
+    gulp.watch('./client/*.js', ['js']);
+    nodemon({
+        script: './server/app.js',
+        ext: 'js',
+        ignore: ['./hosted/*.js', './client/*.js'],
+        tasks: ['lint']
+    });
+});
